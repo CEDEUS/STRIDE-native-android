8e339ba14f3bf1546701826b7acb6fd17980c230
Oscar Plaza Guzmàn <oscarent2@gmail.com>
2018-07-26 14:59:32 -0400

    ADD:SYSTEM 4 SUM HOUR

---
+++ app/src/main/java/com/example/oscarplaza/stride/ListFragmentinSeccion.java
@@ -0,0 +1,29 @@
+package com.example.oscarplaza.stride;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListFragmentinSeccion extends Fragment {
+
+
+    public ListFragmentinSeccion() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_fragmentin_seccion, container, false);
+    }
+
+}
