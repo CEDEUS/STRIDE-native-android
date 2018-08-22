3a4438bd4a7cfdb979eb045efa2ac214b37791c1
osplaza32 <oscarent2@gmail.com>
2018-08-22 13:40:25 -0300

    add fragment to list

---
+++ app/src/main/java/com/example/oscarplaza/stride/ListFragmentandDelete.java
@@ -0,0 +1,41 @@
+package com.example.oscarplaza.stride;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class ListFragmentandDelete extends SupportMapFragment
+{
+    public ListFragmentandDelete(){
+        // empty
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
+        return super.onCreateView(layoutInflater, viewGroup, bundle);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();   
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
